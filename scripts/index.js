e246b282b83906aacae76a4ec799ece53ba15621
Adarsh Pratap Singh <adarsh.lrnr@gmail.com>
2023-03-22 14:45:37 +0530

    completed sidebar

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+const ham = document.querySelector(".ham");
+const sidebar = document.querySelector(".sidebar");
+const search = document.querySelector(".search-bar");
+const user = document.querySelector(".user-info");
+const angle = document.querySelector(".angle-right");
+const bottom = document.querySelector(".bottom");
+const avatar = document.querySelector(".avatar");
+const mainContent = document.querySelector(".main-content");
+
+ham.addEventListener("click", function () {
+	sidebar.classList.toggle("active-sidebar");
+	console.log("working");
+	search.classList.toggle("remove");
+	user.classList.toggle("remove");
+	angle.classList.toggle("remove");
+	bottom.classList.toggle("active-user");
+	avatar.classList.toggle("active-avatar");
+	mainContent.classList.toggle("active-main-content");
+});
